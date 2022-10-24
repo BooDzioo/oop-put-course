d8b9395c8842dfb940bf137bc9b9e4878699e684
Hubert Bogdański <h.bogdanski@expansio.pl>
2022-10-24 16:10:55 +0200

    Add ex-1.cpp file with exercise solution.

---
+++ lesson-2/ex-1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+class Computer {
+    int price, width, height, depth;
+
+    public:
+        Computer() {
+            price = 1000;
+            width = 100;
+            height = 100;
+            depth = 100;
+        }
+
+        Computer(int _price, int _width, int _height, int _depth) {
+            price = _price;
+            width = _width;
+            height = _height;
+            depth = _depth;
+        }
+};
+
+int main() {
+    Computer test = Computer(1, 5000, 100, 100);
+    Computer test2 = Computer();
+
+    return 0;
+}
